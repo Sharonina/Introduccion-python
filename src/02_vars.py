3ed1185aa50e4af74751cbfe2176a3aa2e0cf179
Sharon Arana <shar_mdza@hotmail.com>
2023-04-13 11:23:32 -0600

    Creación, asignación y reasignación de variables

---
+++ src/02_vars.py
@@ -0,0 +1,12 @@
+# Creación de una variable
+my_name = 'Sharon'
+my_age = 26
+
+# Uso de la variable
+print(my_name) # Sharon
+print(my_age) # 26
+
+# Asignación de un nuevo valor a una variale
+my_name = 'Gizela'
+
+print("Aqui cambia el nombre a", my_name) #Gizela
