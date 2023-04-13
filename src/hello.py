66c706bd72de892fb878ef0c7a3b5421e6815dde
Sharon Arana <shar_mdza@hotmail.com>
2023-04-13 11:09:11 -0600

    creación de comentarios

--- src/hello.py
+++ src/hello.py
@@ -1,9 +1,19 @@
+# strings
 print('Hola, esto es python')
-
 print('Hola soy Sharon y tengo 25 años')
 
+
+# operaciones matemáticas
 print(2*2)
 print (2+10)
 print(8/2)
 print(10*10)
 print(10%2)
+
+# Hola, soy un comentario en python :)
+
+"""
+Comentarios a varias lineas son posibles con:
+- 3 comillas simples
+- 3 comillas dobles
+"""
