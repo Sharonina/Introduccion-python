130229f1f80f25d074e9e5a836f3ebdf74572499
Sharon Arana <shar_mdza@hotmail.com>
2023-04-13 19:04:55 -0600

    Booleanos y transformacion de los mismos

---
+++ src/06_booleans.py
@@ -0,0 +1,9 @@
+is_single = True
+print(type(is_single)) # bool
+
+is_single = False
+print(type(is_single)) # bool
+
+# Transformación de booleanos
+print(not True) # False
+print(not False) # True
