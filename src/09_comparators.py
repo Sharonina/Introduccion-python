7de1146b31e5cca65ceb28b04fff53a4a3c98422
Sharon Arana <shar_mdza@hotmail.com>
2023-04-14 10:16:40 -0600

    Uso de comparadores

---
+++ src/09_comparators.py
@@ -0,0 +1,37 @@
+# >
+print(7 > 3)  # True
+print(3 > 7)  # False
+print(7 > 7)  # False
+
+# <
+print(5 < 6)  # True
+print(6 < 5)  # False
+print(5 < 5)  # False
+
+# >=
+print(2 >= 1)  # True
+print(2 >= 3)  # False
+print(2 >= 2)  # True
+
+age = 15
+print(age >= 18)  # False
+age = 19
+print(age >= 18)  # True
+
+# <=
+print(1 <= 2)  # True
+print(2 <= 1)  # False
+print(2 <= 2)  # True
+
+# ==
+print(6 == 6)  # True
+print(5 == 2)  # False
+
+print('apple' == 'apple')  # True
+print('apple' == 'Apple')  # False
+print('1' == 1)  # False
+
+
+# !=
+print(6 != 10)  # True
+print(6 != 6)  # False
