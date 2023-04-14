0087bc1b21f95ac1d2c8b89d3dde391adb5132c6
Sharon Arana <shar_mdza@hotmail.com>
2023-04-13 19:54:32 -0600

    Operadores aritméticos

---
+++ src/08_arithm.py
@@ -0,0 +1,14 @@
+# operadores aritméticos
+
+# Operadores en números
+print(10 + 10) # 20
+print(10 - 5) # 5
+print(10 * 5) # 50
+print(10 / 5) # 2
+print(10 % 2) # 0
+print(10 // 3) # 3
+print(2 ** 3) # 8
+
+# Operadores en strings
+print('Hola ' + 'mundo') # Hola mundo
+print('Hola! ' * 3) # Hola! Hola! Hola!
